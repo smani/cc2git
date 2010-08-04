535111610b16bd30433b670138f0b0b28017f6a6
Marek Langiewicz <marek.langiewicz@softexor.com>
2010-08-04 10:51:04 +0200

    little changes

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import sys
 import os
 import popen2
 import common
-import pprint
+from pprint import pprint
 
 def tshell():
     os.system("A=bla ; B=blee ; echo test1: A=$A B=$B ; export")
@@ -42,9 +42,6 @@ def tcommands():
     print "output:"
     print out
 
-def tassert_raises():
-    common.run_command("dupa")
-
 def tfixme_try_command_out():
         
     lines = common.try_command_out("ls", trials=3, pause=1).splitlines()
@@ -63,12 +60,47 @@ def ttime_str():
     for x in l:
         print x, common.time_str(x)
        
+def walk_test(arg, dir, files):
+    print "walk_test: arg:", arg, "dir:", dir
+    print "files:"
+    for file in files:
+        print file
+    print
+
+def twalk():
+    lotnicza = True
+    stara_lotnicza = False
+
+    #wersja z laptopa (zmodyfikowana lekutko do nowych testow na lotniczej):
+    VIEW = "MAREK_PORTA"
+    full_vobs_dir = "/view/" + VIEW + "/vobs"
+    if lotnicza:
+        full_vobs_dir = "/home/marek\.langiewicz/tmp/walktest/vobs"
+    porta_alternative = "$|(_tools)|(_kernel($|(_2_4_31)))"
+    alternative = "$|(/components)|(/common_software)|(/danube_tc)|(/porta(" + porta_alternative + "))"
+    exclude = "^((?!"+ full_vobs_dir +")|(" + full_vobs_dir + "(?!" + alternative + ")))"                
+    #exclude = "((.*/){9})|(lost\\+found)|(" + exclude + ")"
+    exclude = "(lost\\+found)|(" + exclude + ")"
+
+    if stara_lotnicza:
+        #wersja ze starych testow na lotniczej (mniejwiecej)
+        #full_vobs_dir = "/view/" + VIEW + "/vobs"
+        full_vobs_dir = "/home/marek.langiewicz/tmp/walktest/vobs"
+        porta_alternative = "$|(_tools)|(_kernel($|(_2_4_31)))"
+        alternative = "$|(/components)|(/common_software)|(/danube_tc)|(/porta(" + porta_alternative + "))"
+        exclude = "^((?!"+ full_vobs_dir +")|(" + full_vobs_dir + "(?!" + alternative + ")))"                
+        exclude = "((.*/){8})|(lost\\+found)|(" + exclude + ")"
+
+    common.walk_exclude("/home/langiewi_m/tmp/walktest/", walk_test, "niewazne", exclude)
+    exclude = "a^"
+    common.walk_exclude("/home/langiewi_m/tmp/walktest/vobs/porta/sw/develop/source/siemens/applications/html", walk_test, "niewazne", exclude)
+
 if __name__ == "__main__":
-    tshell()
-    tpopen2()
-    tcommands()
-    tassert_raises()
-    tfixme_try_command_out()
+    #tshell()
+    #tpopen2()
+    #tcommands()
+    #tfixme_try_command_out()
     ttime_str()
+    twalk()
 
     
